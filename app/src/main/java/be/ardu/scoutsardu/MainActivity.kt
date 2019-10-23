9c5949206abfb6513010c5e4beed7592ffa4ca4d
Lucas Vermeulen <lucas.vermeulen@student.hogent.be>
2019-10-23 11:55:42 +0200

    adding safeargs

--- app/src/main/java/be/ardu/scoutsardu/MainActivity.kt
+++ app/src/main/java/be/ardu/scoutsardu/MainActivity.kt
@@ -3,6 +3,8 @@ package be.ardu.scoutsardu
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import be.ardu.scoutsardu.databinding.ActivityMainBinding
 
 
@@ -11,9 +13,17 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val user: User = User()
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        var navController = this.findNavController(R.id.navHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        var navController = this.findNavController(R.id.navHostFragment)
+        return navController.navigateUp()
     }
 }
